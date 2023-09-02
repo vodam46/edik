d1bd6412f1f839e064a3689237bfd719a86a4c34
vodam46 <adamnuc@email.cz>
2023-09-02 13:44:22 +0200

    improved C NEW send_message function string add and remove functions
    IMPROVED
    draw function now supports diacritics, is however very inneficient
    asks for file to open if no cli args
    scrolling - offscreen
    external command execution
    searching function
    get_user_input function
    
    OTHER
    moved all data into editor class
    split screen into 2 windows

--- c/edik.c
+++ c/edik.c
@@ -1,3 +1,5 @@
+#include <wchar.h>
+#include <locale.h>
 #include <ncurses.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,6 +15,15 @@ typedef struct {
 	int x, y;
 } vector;
 
+typedef struct {
+	char** lines;
+	char* filename;
+	vector pos;
+	vector scr_size;
+	int line_num;
+	int scroll_offset;
+} buffer;
+
 void end() {
 	nocbreak();
 	echo();
@@ -21,263 +32,347 @@ void end() {
 	keypad(stdscr, FALSE);
 	endwin();
 }
-void start() {
+void start(vector* scr_size, WINDOW** main_scr, WINDOW** message_scr) {
 	initscr();
 	cbreak();
 	noecho();
 	nonl();
 	raw();
 	keypad(stdscr, TRUE);
+	getmaxyx(stdscr, scr_size->y, scr_size->x);
+	(*main_scr) = newwin(scr_size->y-1, scr_size->x, 0, 0);
+	keypad((*main_scr), TRUE);
+	(*message_scr) = newwin(1, scr_size->x, scr_size->y-1, 0);
+	keypad((*message_scr), TRUE);
 }
 
-void draw(char** lines, int scroll_offset, int line_num, vector scr_size) {
-	clear();
-	for (int y = 0, i = scroll_offset; i <= scr_size.y+scroll_offset; y++, i++) {
-		if (i < line_num) {
-			for (int x = 0, j = 0; j < strlen(lines[i]); x++, j++) {
-				if (lines[i][j] != '\t')
-					mvaddch(y, x, lines[i][j]);
+char* add_to_line(char* old_line, char ch, int x) {
+	char* line = malloc(strlen(old_line));
+	strncpy(line, old_line, x);
+	line[x]=ch;
+	line[x+1]=0;
+	strcat(line, &old_line[x]);
+	old_line = malloc(strlen(line));
+	strcpy(old_line, line);
+	old_line[strlen(line)] = 0;
+	free(line);
+	return old_line;
+}
+
+char* remove_char(char* line, int x) {
+	for (int i = x-1; i < (int)strlen(line); i++) {
+		line[i] = line[i+1];
+	}
+	line = realloc(line, strlen(line));
+	return line;
+}
+
+void draw(buffer edik, WINDOW* main_scr) {
+	wclear(main_scr);
+	for (
+			int scr_y = 0, lines_y = edik.scroll_offset;
+			lines_y <= edik.scr_size.y+edik.scroll_offset;
+			scr_y++, lines_y++
+		) {
+		if (lines_y < edik.line_num) {
+			for (
+					int scr_x = 0, lines_x = 0;
+					lines_x < (int)strlen(edik.lines[lines_y]);
+					scr_x++, lines_x++
+				) {
+				if (edik.lines[lines_y][lines_x] != '\t')
+					mvwaddstr(main_scr, scr_y, 0, edik.lines[lines_y]);
 				else
-					x+=3;
+					scr_x+=3;
 			}
 		}
 	}
-	refresh();
+	wrefresh(main_scr);
 }
 
-char** add_to_lines(char** lines, char ch, vector pos) {
-	char* line = malloc(strlen(lines[pos.y])+sizeof(ch));
-	strncpy(line, lines[pos.y], pos.x);
-	line[pos.x]=ch;
-	line[pos.x+1]=0;
-	strcat(line, &lines[pos.y][pos.x]);
-	lines[pos.y] = malloc(strlen(line)+sizeof(char));
-	strcpy(lines[pos.y], line);
-	lines[pos.y][strlen(line)] = 0;
-	free(line);
-	return lines;
-}
-
-void save(char* filename, char** lines, int line_num) {
-	FILE* file = fopen(filename, "w");
-	file = freopen(filename, "a", file);
+void save(buffer edik) {
+	FILE* file = fopen(edik.filename, "w");
+	file = freopen(edik.filename, "a", file);
 	endwin();
-	for (int i = 0; i < line_num; i++) {
-		fwrite(lines[i], 1, strlen(lines[i]), file);
+	for (int i = 0; i < edik.line_num; i++) {
+		fwrite(edik.lines[i], 1, strlen(edik.lines[i]), file);
 	}
 	fclose(file);
 }
 
-vector find_next(char** lines, char* query, vector pos, int line_num) {
-	for (int i = pos.y; i < line_num; i++) {
-		char* needle = strstr(&lines[i][pos.x+1], query);
-		if (needle !=NULL) {
-			int new_x = needle-lines[i];
-			if (new_x != pos.x)
-				return (vector){new_x, i};
+vector find_word(buffer edik, char* query, int offset) {
+	vector new_pos;
+	new_pos = (vector){edik.pos.x+offset, edik.pos.y};
+	if (offset == -1) {
+		if (new_pos.x < 0) {
+			new_pos.y--;
+			if (new_pos.y >= 0)
+				new_pos.x = strlen(edik.lines[edik.pos.y])-1;
 		}
 	}
-	return pos;
-}
-vector find_prev(char** lines, char* query, vector pos, int line_num) {
-	for (int i = pos.y; i > 0; i--) {
-		char* needle = strstr(lines[i], query);
-		if (needle !=NULL) {
-			return (vector){needle-lines[i], i};
+	for (int y = new_pos.y; y  < edik.line_num && y >= 0; y += offset) {
+		for (int i = new_pos.x;
+				i < (int)strlen(edik.lines[y]) && i >= 0;
+				i += offset) {
+			int loop = 1;
+			for (int j = 0; j  < (int)strlen(query); j++) {
+				if (edik.lines[y][i+j] != query[j]) {
+					loop = 0;
+					new_pos.y += offset;
+					if (offset == 1)
+						new_pos.x = 0;
+					else
+						if (new_pos.y >= 0)
+							new_pos.x = strlen(edik.lines[new_pos.y])-1;
+					break;
+				}
+			}
+			if (loop) {
+				if (
+						i >= 0 && i <= (int)strlen(edik.lines[y])
+						&& y >= 0 && y < edik.line_num
+				   ) return (vector){i, y};
+			}
 		}
 	}
-	return pos;
+	return edik.pos;
 }
 
-char* get_user_input(char* prompt, vector scr_size) {
-	move(scr_size.y-1, 0);
-	refresh();
-	printw("%s", prompt);
+void send_message(WINDOW* message_scr, char* fmt, ...) {
+	va_list args;
+	wclear(message_scr);
+	va_start(args, fmt);
+	wmove(message_scr, 0, 0);
+	vw_printw(message_scr, fmt, args);
+	va_end(args);
+	wrefresh(message_scr);
+}
+
+char* get_user_input(char* prompt, WINDOW* message_scr) {
+	send_message(message_scr, "%s", prompt);
 	char* ret_str = malloc(sizeof(char));
 	ret_str[0] = 0;
 	int* ch = malloc(sizeof(int));
-	ch[0] = getch();
-	while (ch[0] != '\r' && ch[0] != '\n' && ch[0] != KEY_BREAK) {
-		addch(ch[0]);
-		strcat(ret_str, (char*)ch);
-		ret_str = realloc(ret_str, strlen(ret_str)+sizeof(char*));
-		ret_str[sizeof(char*)] = 0;
-		ch[0] = getch();
+	int i = 0;
+	while (1) {
+		ch[0] = wgetch(message_scr);
+		switch (ch[0]) {
+			case '\r':
+			case '\n':
+			case KEY_BREAK:
+				return ret_str;
+			case 27:
+			case CTRL('c'):
+			case CTRL('q'):
+				return "";
+			case KEY_BACKSPACE:
+				if (i != 0) {
+					ret_str = remove_char(ret_str, i);
+					i--;
+				}
+				if (i == 0) {
+					ret_str = "";
+				}
+				break;
+			default:
+				ret_str = add_to_line(ret_str, ch[0], i);
+				i++;
+		}
+		send_message(message_scr, "%s%s", prompt, ret_str);
 	}
-	return ret_str;
 }
 
 int main(int argc, char** argv) {
-	if (argc != 2) {
-		printf("wrong arguments, creating new files is not implemented yet\n");
+	setlocale(LC_ALL, "");
+	buffer edik = {0};
+	WINDOW* main_scr = {0};
+	WINDOW* message_scr = {0};
+	start(&edik.scr_size, &main_scr, &message_scr);
+	atexit(end);
+
+	FILE* file;
+	if (argc == 1) {
+		edik.filename = get_user_input("Choose filename: ", message_scr);
+		if (strlen(edik.filename) == 0) {
+			end();
+			printf("Empty filenames are not possible\n");
+			return 1;
+		}
+	} else if (argc == 2) {
+		edik.filename = argv[1];
+
+	} else {
+		end();
+		fprintf(stderr, "Wrong arguemnts\n");
 		return 1;
 	}
-	char* filename = argv[1];
-	FILE* file = fopen(filename, "r");
+	file = fopen(edik.filename, "r");
 	if (file == NULL) {
-		printf("error when reading file\n");
-		return 1;
+		fclose(fopen(edik.filename, "a+"));
+		file = fopen(edik.filename, "r");
 	}
 
-	int line_num = 0;
 	while (!feof(file)) {
 		int ch = fgetc(file);
 		if (ch == '\n') {
-			line_num++;
+			edik.line_num++;
 		}
 	}
 	unsigned int fsize = ftell(file);
 	rewind(file);
-	char** lines;
 	if (fsize != 0) {
-		lines = malloc(sizeof(char*) * line_num);
+		edik.lines = malloc(sizeof(char*) * edik.line_num);
 		size_t len;
-		for (int i = 0; i < line_num; i++) {
-			getline(&lines[i], &len, file);
+		for (int i = 0; i < edik.line_num; i++) {
+			getline(&edik.lines[i], &len, file);
 		}
 	} else {
-		line_num++;
-		lines = malloc(sizeof(char*) * line_num);
-		lines[0] = "\n\0";
+		edik.line_num++;
+		edik.lines = malloc(sizeof(char*) * edik.line_num);
+		edik.lines[0] = "\n\0";
 	}
 	fclose(file);
 
-	start();
-	atexit(end);
-
-	vector scr_size;
-	getmaxyx(stdscr, scr_size.y, scr_size.x);
-
-	vector pos = {0,0};
-	int loop = 1;
+	edik.pos = (vector){0,0};
+	short int loop = 1;
 
 	char* search_str;
 	char* command_string;
-	int scroll_offset = 0;
 	while (loop) {
-		draw(lines, scroll_offset, line_num, scr_size);
+		draw(edik, main_scr);
+		send_message(message_scr, "%s", edik.filename);
 		int tab_offset = 0;
-		for (int i = 0; i < pos.x; i++) {
-			if (lines[pos.y][i] == '\t') {
+		for (int i = 0; i < edik.pos.x; i++) {
+			if (edik.lines[edik.pos.y][i] == '\t') {
 				tab_offset += 3;
 			}
 		}
-		move(pos.y - scroll_offset, pos.x + tab_offset);
-		int ch = getch();
+		wmove(main_scr, edik.pos.y - edik.scroll_offset, edik.pos.x + tab_offset);
+		int ch = wgetch(main_scr);
 		switch (ch) {
-
 			case KEY_RESIZE:
-				getmaxyx(stdscr, scr_size.y, scr_size.x);
+				getmaxyx(stdscr, edik.scr_size.y, edik.scr_size.x);
 				break;
 
 			case CTRL('q'):
 				loop=0;
 				break;
 			case CTRL('s'):
-				save(filename, lines, line_num);
+				save(edik);
 				break;
 
 			case CTRL('f'):
-				search_str = get_user_input("Search: ", scr_size);
+				search_str = get_user_input("Search: ", message_scr);
 				break;
 			case CTRL('n'):
-				pos = find_next(lines, search_str, pos, line_num);
+				edik.pos = find_word(edik, search_str, 1);
 				break;
 			case CTRL('p'):
-				pos = find_prev(lines, search_str, pos, line_num);
+				edik.pos = find_word(edik, search_str, -1);
 				break;
 
 			case CTRL('x'):
-				command_string = get_user_input("Command: ", scr_size);
-				end();
-				printf("%s\n", command_string);
-				system(command_string);
-				printf("\n\nPress any key to continue");
-				getchar();
-				start();
+				command_string = get_user_input("Command: ", message_scr);
+				if (strlen(command_string)) {
+					end();
+					printf("%s\n", command_string);
+					printf("\n%d\n\nPress Enter to continue", system(command_string));
+					getchar();
+					start(&edik.scr_size, &main_scr, &message_scr);
+				}
 				break;
 
 			case KEY_LEFT:
-				pos.x = MAX(0, pos.x - 1);
+				edik.pos.x = MAX(0, edik.pos.x - 1);
 				break;
 			case KEY_RIGHT:
-				pos.x = MIN(strlen(lines[pos.y])-1, pos.x+1);
+				edik.pos.x = MIN((int)strlen(edik.lines[edik.pos.y])-1, edik.pos.x+1);
 				break;
 			case KEY_DOWN:
-				pos.y = MIN(line_num-1, pos.y+1);
-				pos.x = MIN(strlen(lines[pos.y])-1, pos.x);
-				if (pos.y - scroll_offset >= scr_size.y) {
-					scroll_offset++;
-				}
+				edik.pos.y = MIN(edik.line_num-1, edik.pos.y+1);
+				edik.pos.x = MIN((int)strlen(edik.lines[edik.pos.y])-1, edik.pos.x);
 				break;
 			case KEY_UP:
-				if (pos.y - scroll_offset <= 0) {
-					scroll_offset = MAX(scroll_offset-1, 0);
-				}
-				pos.y = MAX(0, pos.y - 1);
-				pos.x = MIN(strlen(lines[pos.y])-1, pos.x);
+				edik.pos.y = MAX(0, edik.pos.y - 1);
+				edik.pos.x = MIN((int)strlen(edik.lines[edik.pos.y])-1, edik.pos.x);
 				break;
 
 			case KEY_BREAK:
 			case '\n':
 			case '\r':
-				line_num++;
-				char* left = malloc((pos.x+1)*sizeof(char));
-				strncpy(left, lines[pos.y], pos.x);
-				left[pos.x] = '\n';
-				left[pos.x+1] = 0;
+				edik.line_num++;
+				char* left = malloc((edik.pos.x+1));
+				strncpy(left, edik.lines[edik.pos.y], edik.pos.x);
+				left[edik.pos.x] = '\n';
+				left[edik.pos.x+1] = 0;
 				char* right;
-				if (pos.x != strlen(lines[pos.y])-1) {
-					right = malloc((strlen(lines[pos.y]) - pos.x)*sizeof(char));
-					strcat(right, &lines[pos.y][pos.x]);
+				if (edik.pos.x != (int)strlen(edik.lines[edik.pos.y])-1) {
+					right = malloc((strlen(edik.lines[edik.pos.y])-edik.pos.x));
+					strcpy(right, &edik.lines[edik.pos.y][edik.pos.x]);
 				} else {
 					right = "\n";
 				}
-				lines = realloc(lines, line_num * sizeof(char*));
-				lines[pos.y] = left;
-				for (int i = line_num-1; i > pos.y; i--) {
-					lines[i] = lines[i-1];
+				edik.lines = realloc(edik.lines, edik.line_num * sizeof(char*));
+				edik.lines[edik.pos.y] = left;
+				for (int i = edik.line_num-1; i > edik.pos.y; i--) {
+					edik.lines[i] = edik.lines[i-1];
 				}
-				lines[pos.y+1] = right;
-				pos.x = 0;
-				pos.y += 1;
+				edik.lines[edik.pos.y+1] = right;
+				edik.pos.x = 0;
+				edik.pos.y += 1;
 				break;
 
 			case KEY_BACKSPACE:
-				if (pos.x != 0) {
-					for (int i = pos.x-1; i < strlen(lines[pos.y]); i++) {
-						lines[pos.y][i] = lines[pos.y][i+1];
-					}
-					lines[pos.y] = realloc(lines[pos.y], strlen(lines[pos.y]));
-					pos.x--;
-				} else if (pos.y != 0) {
-					int new_x = strlen(lines[pos.y-1]);
+				if (edik.pos.x != 0) {
+					edik.lines[edik.pos.y] = remove_char(
+						edik.lines[edik.pos.y],
+						edik.pos.x
+					);
+					edik.pos.x--;
+				} else if (edik.pos.y != 0) {
+					int new_x = strlen(edik.lines[edik.pos.y-1]);
 					char* new_line = malloc(
-							(new_x+strlen(lines[pos.y])+1)*sizeof(char)
-							);
+						(new_x+strlen(edik.lines[edik.pos.y])+1)
+					);
 					new_line[0] = 0;
-					strcat(new_line, lines[pos.y-1]);
+					strcat(new_line, edik.lines[edik.pos.y-1]);
 					new_line[strlen(new_line)-1] = 0;
-					strcat(new_line, lines[pos.y]);
-					for (int i = pos.y; i < line_num; i++) {
-						lines[i] = lines[i+1];
+					strcat(new_line, edik.lines[edik.pos.y]);
+					for (int i = edik.pos.y; i < edik.line_num; i++) {
+						edik.lines[i] = edik.lines[i+1];
 					}
-					lines[pos.y-1] = new_line;
-					line_num--;
-					lines = realloc(lines, line_num*sizeof(char*));
-					pos.y--;
-					pos.x = new_x-1;
+					edik.lines[edik.pos.y-1] = new_line;
+					edik.line_num--;
+					edik.lines = realloc(edik.lines, edik.line_num);
+					edik.pos.y--;
+					edik.pos.x = new_x-1;
 				}
 				break;
 
 			default:
 				if (ch >= 32) {
-					lines = add_to_lines(lines, ch, pos);
-					pos.x++;
+					edik.lines[edik.pos.y] = add_to_line(
+						edik.lines[edik.pos.y],
+						ch,
+						edik.pos.x
+					);
+					edik.pos.x++;
 				}
 				break;
 		}
+		while (edik.pos.y - edik.scroll_offset >= edik.scr_size.y-1) {
+			edik.scroll_offset++;
+		}
+		while (edik.pos.y - edik.scroll_offset < 0) {
+			edik.scroll_offset = MAX(edik.scroll_offset-1, 0);
+		}
 	}
 	end();
-	free(lines);
+	for (int i = 0; i < edik.line_num; i++) {
+		if (strlen(edik.lines[i]) != 1) {
+			free(edik.lines[i]);
+		}
+	}
+	free(edik.lines);
+	return 0;
 }
